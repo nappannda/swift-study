f1910c8d93cba28730063b43dccd06cfd7a969e3
nappannda <qwe159753qwee@gmail.com>
2018-01-22 21:03:31 +0900

    Finish 2.9

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,14 +1,3 @@
-// 更新
-var dictionary1 = ["key": 1]
-dictionary1["key"] = 2
-dictionary1
-
-// 追加
-var dictionary2 = ["key": 1]
-dictionary2["key2"] = 2
-dictionary2
-
-// 削除
-var dictionary3 = ["key": 1]
-dictionary3["key"] = nil
-dictionary3
+let range = 1...4
+range.contains(2)
+range.contains(5)

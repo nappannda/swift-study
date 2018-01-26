239459be44625642ac00baac6c7dd54b0c5513c0
nappannda <qwe159753qwee@gmail.com>
2018-01-27 04:14:42 +0900

    Finish 3.4

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,6 +1,24 @@
-var a = 1
+// for文の場合
+label: for element in [1, 2, 3] {
+    for nestedElement in [1, 2, 3] {
+        print("for文")
+        break label
+    }
+}
 
-repeat {
-    print(a)
-    a += 1
-} while a < 1
+// while文の場合
+label: while true {
+    while true {
+        print("while文")
+        break label
+    }
+}
+
+
+// repeat文の場合
+label: repeat {
+    repeat {
+        print("repeat文")
+        break label
+    } while true
+} while true

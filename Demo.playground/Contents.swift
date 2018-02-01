1f7f4650cea4955436ae5f8c0ab0f63d3daa0ad4
nappannda <qwe159753qwee@gmail.com>
2018-02-01 20:30:18 +0900

    Finish5.4

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,9 +1,27 @@
-struct Greeting {
-    var to = "Yosuke Ishikawa"
+// 失敗可能イニシャライザの使用
+struct Greeting1 {
+    let to: String
     var body: String {
         return "Hello, \(to)!"
     }
+    
+    init?(dictionary: [String: String]) {
+        guard let to = dictionary["to"] else {
+            return nil
+        }
+        
+        self.to = to
+    }
 }
 
-let greeting = Greeting()
-greeting.body = "Hi"
+// デフォルト値の用意
+struct Greeting2 {
+    let to: String
+    var body: String {
+        return "Hello, \(to)!"
+    }
+    
+    init(dictionary: [String: String]) {
+        to = dictionary["to"] ?? "Yosuke Ishikawa"
+    }
+}

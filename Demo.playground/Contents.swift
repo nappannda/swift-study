889ed3e3d113e44d08017ce01dc888bcb0b815e3
nappannda <qwe159753qwee@gmail.com>
2018-02-03 17:00:19 +0900

    Finish 5.8

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,35 +1,20 @@
-import UIKit
-
-// アプリケーション固有のエラー
-enum WebAPIError : Error {
-    case connectionError(Error)
-    case fatalError
-    
-    var title: String {
-        switch self {
-        case .connectionError:
-            return "通信エラー"
-        case .fatalError:
-            return "致命的エラー"
-        }
+struct NewsFeedItem {
+    enum Kind {
+        case a
+        case b
+        case c
     }
     
-    var message: String {
-        switch self {
-        case .connectionError(let underlyingError):
-            return underlyingError.localizedDescription + "再試行してください"
-        case .fatalError:
-            return "サポート窓口に連絡してください"
-        }
-    }
+    let id: Int
+    let title: String
+    let type: Kind
 }
 
-extension UIAlertController {
-    convenience init(webAPIError: WebAPIError) {
-        // UIAlertControllerの指定イニシャライザ
-        self.init(title: webAPIError.title, message: webAPIError.message, preferredStyle: .alert)
-    }
-}
+let type = NewsFeedItem.Kind.a
+let item = NewsFeedItem(id: 1, title: "Table", type: type)
 
-let error = WebAPIError.fatalError
-let alertController = UIAlertController(webAPIError: error)
+switch item.type {
+case .a: print("type is .a")
+case .b: print("type is .b")
+case .c: print("type is .c")
+}

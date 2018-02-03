9c3c5472fa0956a4e2cffff3a471cf03c0f5195e
nappannda <qwe159753qwee@gmail.com>
2018-02-03 17:44:00 +0900

    Finish 6.3

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,21 +1,15 @@
-class IntBox {
-    var value: Int
+struct Article {
+    let id: Int
+    let title: String
+    let body: String
     
-    init(value: Int) {
-        self.value = value
-    }
+    // 以下と同等のイニシャライザが自動的に定義される
+    // init(id: Int, title: String, body: String) {
+    //
+    // }
 }
 
-var a = IntBox(value: 1) // aはIntBox(value: 1)を参照する
-var b = a // bはaと同じインスタンスを参照する
-
-// a.value, b.valueは両方とも1
-a.value
-b.value
-
-// a.valueを2に変更する
-a.value = 2
-
-// a.value,b.value両方とも2
-a.value
-b.value
+let article = Article(id: 1, title: "Hello", body: "...")
+article.id
+article.title
+article.body

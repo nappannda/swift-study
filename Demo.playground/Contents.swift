b77f3fbe5df812c4b530aadb4ede667338b59e17
nappannda <qwe159753qwee@gmail.com>
2018-02-03 16:48:49 +0900

    Finish 5.7

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,3 +1,35 @@
-let array = [1, 2, 3, 4]
-let element = array[0]
-let slice = array[0...2]
+import UIKit
+
+// アプリケーション固有のエラー
+enum WebAPIError : Error {
+    case connectionError(Error)
+    case fatalError
+    
+    var title: String {
+        switch self {
+        case .connectionError:
+            return "通信エラー"
+        case .fatalError:
+            return "致命的エラー"
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .connectionError(let underlyingError):
+            return underlyingError.localizedDescription + "再試行してください"
+        case .fatalError:
+            return "サポート窓口に連絡してください"
+        }
+    }
+}
+
+extension UIAlertController {
+    convenience init(webAPIError: WebAPIError) {
+        // UIAlertControllerの指定イニシャライザ
+        self.init(title: webAPIError.title, message: webAPIError.message, preferredStyle: .alert)
+    }
+}
+
+let error = WebAPIError.fatalError
+let alertController = UIAlertController(webAPIError: error)

bba86222145bc1c1ade61e5d354c60aa7f19d809
nappannda <qwe159753qwee@gmail.com>
2018-02-04 19:32:12 +0900

    Finish 7.1

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,16 +1,8 @@
-enum Color {
-    case rgb(Float, Float, Float)
-    case cmyk(Float, Float, Float, Float)
+func printIfEqual<T: Equatable>(_ arg1: T, _ arg2: T) {
+    if arg1 == arg2 {
+        print("Both are \(arg1)")
+    }
 }
 
-let rgb = Color.rgb(0.0, 0.33, 0.66)
-let cmyk = Color.cmyk(0.0, 0.33, 0.66, 0.99)
-
-let color = Color.rgb(0.0, 0.33, 0.66)
-
-switch color {
-case .rgb(let r, let g, let b):
-    print("r: \(r), g: \(g), b: \(b)")
-case .cmyk(let c, let m, let y, let k):
-    print("c: \(c), m: \(m), y: \(y), k: \(k)")
-}
+printIfEqual(123, 123)
+printIfEqual("str", "str")

a57b08a0ea28b5c5d9eebfb202173dffcd231f51
nappannda <qwe159753qwee@gmail.com>
2018-02-04 19:05:03 +0900

    Finish 6.4

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,15 +1,18 @@
-struct Article {
-    let id: Int
-    let title: String
-    let body: String
-    
-    // 以下と同等のイニシャライザが自動的に定義される
-    // init(id: Int, title: String, body: String) {
-    //
-    // }
+class SomeClass : Equatable {
+    static func ==(lhs: SomeClass, rhs: SomeClass) -> Bool {
+        return true
+    }
 }
 
-let article = Article(id: 1, title: "Hello", body: "...")
-article.id
-article.title
-article.body
+let a = SomeClass()
+let b = SomeClass()
+let c = a
+
+// 定数aとbは同じ値
+a == b
+
+// 定数aとbの参照先は異なる
+a === b
+
+// 定数aとcの参照先は同じ
+a === c

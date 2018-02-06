1e46a41b5750162c1cb01609cf2f5113bd78b1c2
nappannda <qwe159753qwee@gmail.com>
2018-02-07 05:48:59 +0900

    Finish 8.5

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,6 +1,9 @@
-struct Container<Content> {
-    var content: Content
+func concat<T : Collection, U : Collection>
+    (_ argument1: T, _ argument2: U) -> [T.Iterator.Element]
+    where T.Iterator.Element == U.Iterator.Element {
+        return Array(argument1) + Array(argument2)
 }
 
-let intContainer = Container(content: 1)
-let stringContainer = Container(content: "abc")
+let array = [1, 2, 3]
+let set = Set([1, 2, 3])
+let result = concat(array, set)

01878875d3f199c57c82d1c17d77d3262b948dbf
nappannda <qwe159753qwee@gmail.com>
2018-02-06 22:13:21 +0900

    Finish 8.1

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,32 +1,7 @@
-struct IntCollection : Collection {
-    var startIndex: Int {
-        return 0
-    }
-    
-    var endIndex: Int {
-        return 0
-    }
-    
-    subscript(position: Int) -> Int {
-        return position
-    }
-    
-    func index(after i: Int) -> Int {
-        return i + 1
-    }
+func isEqual<T: Equatable>(_ x: T, _ y: T) -> Bool {
+    return x == y
 }
 
-let collection = IntCollection()
-let element = collection[5]
-
-let count = collection.count
-let first = collection.first
-let isEmpty = collection.isEmpty
-let slice = collection[3...5]
-
-var array = [Int]()
-for element in collection {
-    array.append(element)
-}
-
-array
+isEqual("abc", "def")
+isEqual(1.0, 3.14)
+isEqual(false, false)

aa0a70e782f74aad8cfd93ef2eac56fa7559a5a9
nappannda <qwe159753qwee@gmail.com>
2018-02-07 05:36:09 +0900

    Finish 8.3

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,23 +1,7 @@
-// ジェネリクスを使った関数
-func identityWithGenericValue<T>(_ argument: T) -> T {
-    return argument
+func someFunction<T>(_ any: Any) -> T? {
+    return any as? T
 }
 
-let genericInt = identityWithGenericValue(1)
-let genericString = identityWithGenericValue("abc")
-
-// Anyを使った関数
-func identityWithAnyValue(_ argument: Any) -> Any {
-    return argument
-}
-
-let anyInt = identityWithAnyValue(1)
-let anyString = identityWithAnyValue("abc")
-
-if let int = anyInt as? Int {
-    // ここでようやくInt型として扱えるようになる
-    print("anyInt is \(int)")
-} else {
-    // Int型へのダウンキャストが失敗した場合を考慮する必要がある
-    print("The type of anyInt is not Int")
-}
+let a: String? = someFunction("abc")
+let b: Int? = someFunction(1)
+let c = someFunction("abc")

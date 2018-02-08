b5a26af7ac8d4d807eb917575c11b83f051cd396
nappannda <qwe159753qwee@gmail.com>
2018-02-08 20:34:46 +0900

    Finish 10.1

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,9 +1,19 @@
-func concat<T : Collection, U : Collection>
-    (_ argument1: T, _ argument2: U) -> [T.Iterator.Element]
-    where T.Iterator.Element == U.Iterator.Element {
-        return Array(argument1) + Array(argument2)
+var temporaryData: String?
+
+class SomeClass {
+    init() {
+        print("Create a temporary data")
+        temporaryData = "a temporary data"
+    }
+    
+    deinit {
+        print("Clean up the temporary data")
+        temporaryData = nil
+    }
 }
 
-let array = [1, 2, 3]
-let set = Set([1, 2, 3])
-let result = concat(array, set)
+var someClass: SomeClass? = SomeClass()
+temporaryData
+
+someClass = nil
+temporaryData

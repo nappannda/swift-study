1902b3ba8b769b345db0a57c5115e4fcd7d96900
nappannda <qwe159753qwee@gmail.com>
2018-02-11 15:10:24 +0900

    Finish 12.3

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -3,14 +3,32 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-let quque = DispatchQueue.global(qos: .userInitiated)
-
-quque.async {
-    let isMainThread = Thread.isMainThread
-    print("非同期の処理")
-    let quque = DispatchQueue.main
-    quque.async {
-        let isMainThread = Thread.isMainThread
-        print("メインスレッドでの処理")
+class SomeOperation : Operation {
+    let number: Int
+    
+    init(number: Int) { self.number = number }
+    
+    override func main() {
+        Thread.sleep(forTimeInterval: 1)
+        
+        if isCancelled { return }
+        
+        print(number)
     }
 }
+
+let quequ = OperationQueue()
+quequ.name = "com.example.my_operation_quque"
+quequ.maxConcurrentOperationCount = 2
+quequ.qualityOfService = .userInitiated
+
+var operations = [SomeOperation]()
+
+for i in 0..<10 {
+    operations.append(SomeOperation(number: i))
+    if i > 0 {
+        operations[i].addDependency(operations[i-1])
+    }
+}
+
+quequ.addOperations(operations, waitUntilFinished: false)

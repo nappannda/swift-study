d2febaa9fe4ef1f9bf7a0c47bc799d655da31c3f
nappannda <qwe159753qwee@gmail.com>
2018-02-11 15:25:04 +0900

    Finish 12.4

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,34 +1,10 @@
 import Foundation
-import PlaygroundSupport
 
-PlaygroundPage.current.needsIndefiniteExecution = true
-
-class SomeOperation : Operation {
-    let number: Int
-    
-    init(number: Int) { self.number = number }
-    
+class SomeThread: Thread {
     override func main() {
-        Thread.sleep(forTimeInterval: 1)
-        
-        if isCancelled { return }
-        
-        print(number)
+        print("executed.")
     }
 }
 
-let quequ = OperationQueue()
-quequ.name = "com.example.my_operation_quque"
-quequ.maxConcurrentOperationCount = 2
-quequ.qualityOfService = .userInitiated
-
-var operations = [SomeOperation]()
-
-for i in 0..<10 {
-    operations.append(SomeOperation(number: i))
-    if i > 0 {
-        operations[i].addDependency(operations[i-1])
-    }
-}
-
-quequ.addOperations(operations, waitUntilFinished: false)
+let thread = SomeThread()
+thread.start()

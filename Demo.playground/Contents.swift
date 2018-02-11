c6ccae2f5bd6ec1f94e1f8d3d2f2c481d76f71eb
nappannda <qwe159753qwee@gmail.com>
2018-02-12 01:05:12 +0900

    Finish 13.6

--- Demo.playground/Contents.swift
+++ Demo.playground/Contents.swift
@@ -1,12 +1,18 @@
-func title(forButtonAt index: Int) -> String {
-    switch index {
-    case 0:
-        return "赤"
-    case 1:
-        return "青"
-    case 2:
-        return "黄"
+func printSeason(forMonth month: Int) {
+    switch month {
+    case 1...2, 12:
+        print("冬")
+    case 3...5:
+        print("春")
+    case 6...8:
+        print("夏")
+    case 9...11:
+        print("秋")
     default:
-        fatalError("想定外のボタンのインデックス\(index)を受け取りました。")
+        assertionFailure("monthには1から12までの値を設定してください")
     }
 }
+
+printSeason(forMonth: 11)
+printSeason(forMonth: 12)
+printSeason(forMonth: 13)
